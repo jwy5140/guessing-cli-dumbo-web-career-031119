379863656e97961c212b775eb5acf65259c8193e
Jeffrey Yu <jwy5140@gmail.com>
2019-03-06 07:02:29 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     ans = gets.chomp
     case ans 
     when "exit"
-      return "Goodbye"
+      return "Goodbye!"
     when ans.to_i === comp_guess
       puts "You guessed the correct number!"
     else
